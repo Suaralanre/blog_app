9cba82f4842575d8f780930c37c698a5a559de89
Suaralanre <sirstuffy@gmail.com>
2023-06-17 17:26:52 +0100

    Updated with CRUD functionality

--- blog/tests.py
+++ blog/tests.py
@@ -7,7 +7,7 @@ from .models import Post
 class BlogTests(TestCase):
 
     @classmethod
-    def setUpClass(cls) -> None:
+    def setUpTestData(cls) -> None:
         cls.user = get_user_model().objects.create_user(
             username="testuser", email="test@email.com", password="secret"
         )
@@ -46,3 +46,33 @@ class BlogTests(TestCase):
         self.assertEqual(no_response.status_code, 404)
         self.assertContains(response, "A good title")
         self.assertTemplateUsed(response, "post_detail.html")
+
+    def test_post_createview(self):
+        response = self.client.post(
+            reverse("post_new"),
+            {
+                "title": "New title",
+                "body": "New text",
+                "author": self.user.id,
+            }
+        )
+        self.assertEqual(response.status_code, 302)
+        self.assertEqual(Post.objects.last().title, "New title")
+        self.assertEqual(Post.objects.last().body, "New text")
+    
+    def test_post_updateview(self):
+        response = self.client.post(
+            reverse("post_edit", args="1"),
+            {
+                "title": "Updated title",
+                "body": "Updated text",
+            }
+        )
+        self.assertEqual(response.status_code, 302)
+        self.assertEqual(Post.objects.last().title, "Updated title")
+        self.assertEqual(Post.objects.last().body, "Updated text")
+    
+    def test_post_deleteview(self):
+        response = self.client.post(
+            reverse("post_delete", args="1"))
+        self.assertEqual(response.status_code, 302)
